8ecb318a9e79d5c3bd22c58da9e5c51e8a2f892d
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 16:45:06 +0700

    Create Spring Boot Project 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/testcontainers/SpringbootTestcontainersApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.testcontainers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestcontainersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootTestcontainersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/my/hendisantika/testcontainers/SpringbootTestcontainersApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.testcontainers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootTestcontainersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
