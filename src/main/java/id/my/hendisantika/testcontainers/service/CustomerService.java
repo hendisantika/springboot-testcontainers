b17a8c28ac3f8d6affd858bfb0197972bd9cef6f
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 16:54:54 +0700

    Add findByName in CustomerService 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/testcontainers/service/CustomerService.java
+++ src/main/java/id/my/hendisantika/testcontainers/service/CustomerService.java
@@ -1,10 +1,12 @@
 package id.my.hendisantika.testcontainers.service;
 
+import id.my.hendisantika.testcontainers.config.RedisConfig;
 import id.my.hendisantika.testcontainers.dto.CustomerDTO;
 import id.my.hendisantika.testcontainers.publisher.CustomerEventPublisher;
 import id.my.hendisantika.testcontainers.repository.CustomerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
@@ -31,6 +33,13 @@ public class CustomerService {
 
     public List<CustomerDTO> findAll() {
         return customerRepository.findAll().stream().map(customer ->
+                new CustomerDTO(customer.getName(), customer.getId())).toList();
+    }
+
+    @Cacheable(RedisConfig.CUSTOMER_CACHE)
+    public List<CustomerDTO> findByName(String name) {
+        log.info("------ Hitting database & not using cache! ------ ");
+        return customerRepository.findByNameIgnoreCase(name).stream().map(customer ->
                 new CustomerDTO(customer.getName(), customer.getId())).toList();
     }
 }
