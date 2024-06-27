e776d55363b2b295265468df080067dbab8162ee
hendisantika <hendisantika@yahoo.co.id>
2024-06-27 16:58:50 +0700

    Add data.sql file🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/testcontainers/data.sql
@@ -0,0 +1,15 @@
+DELETE
+FROM customer;
+
+INSERT INTO customer(name)
+VALUES ('Yuji');
+INSERT INTO customer(name)
+VALUES ('Gojo');
+INSERT INTO customer(name)
+VALUES ('Megumi');
+INSERT INTO customer(name)
+VALUES ('Geto');
+INSERT INTO customer(name)
+VALUES ('Maki');
+INSERT INTO customer(name)
+VALUES ('Jogo');
