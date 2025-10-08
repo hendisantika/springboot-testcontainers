be3da61fe0d0e0462be5ad9bf50b7d4a53b4e3a8
hendisantika <hendisantika@yahoo.co.id>
2025-10-08 19:43:25 +0700

    feat: Add Spring Docker Compose Support 🫸🌀✏️📗🐧🐳⬆

--- src/test/java/id/my/hendisantika/testcontainers/config/TestContainerConfig.java
+++ src/test/java/id/my/hendisantika/testcontainers/config/TestContainerConfig.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.testcontainers.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.jdbc.JdbcConnectionDetails;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ResourceLoader;
+import org.testcontainers.containers.wait.strategy.Wait;
 import org.testcontainers.utility.DockerImageName;
 
 /**
@@ -24,4 +28,29 @@ public class TestContainerConfig {
 
     @Autowired
     private ResourceLoader resourceLoader;
+
+    @Bean
+    // This annotation is used to indicate that this bean will not be re-created if the application
+    // restarts due to spring-dev-tools.
+    // @org.springframework.boot.devtools.restart.RestartScope
+    // By default, ServiceConnection will create all applicable connection details beans
+    // for a given Container.
+    // For example, a PostgreSQLContainer will create both JdbcConnectionDetails and R2dbcConnectionDetails.
+    @ServiceConnection(type = JdbcConnectionDetails.class)
+    public PostgresContainer postgresContainer() {
+
+        final long memoryInBytes = 64L * 1024L * 1024L;
+        final long memorySwapInBytes = 128L * 1024L * 1024L;
+
+        return new PostgresContainer(POSTGRES_IMAGE)
+                .waitingFor(Wait.forLogMessage(".*PostgreSQL init process complete;.*\\n", 1))
+                // The Reusable Containers feature keeps the same containers running between test sessions
+                .withReuse(true)
+                .withCreateContainerCmdModifier(cmd -> {
+                    cmd.withName("postgres");
+                    cmd.getHostConfig()
+                            .withMemory(memoryInBytes)
+                            .withMemorySwap(memorySwapInBytes);
+                });
+    }
 }
---
+++ src/test/java/id/my/hendisantika/testcontainers/config/TestContainerLogConfig.java
@@ -0,0 +1,58 @@
+package id.my.hendisantika.testcontainers.config;
+
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Service;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.utility.DockerImageName;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : testcontainers
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/06/24
+ * Time: 17.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+@Service
+public class TestContainerLogConfig {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestContainerLogConfig.class);
+
+    static {
+        GenericContainer<?> redis =
+                new GenericContainer<>(DockerImageName.parse("redis:7.2.5-alpine3.20")).withExposedPorts(6379);
+        redis.start();
+    }
+
+    @Autowired
+    private PostgreSQLContainer postgresContainer;
+    @Autowired
+    private KafkaContainer kafkaContainer;
+
+    @Autowired
+    private PythonContainer pythonContainer;
+
+    @PostConstruct
+    public void init() {
+
+        // Attaching the log of the Containers we create to the log of the Spring-Boot app.
+        Slf4jLogConsumer logConsumer = new Slf4jLogConsumer(LOGGER);
+        logConsumer.withPrefix("TC-LOG--->");
+
+        postgresContainer.followOutput(logConsumer);
+        redisContainer.followOutput(logConsumer);
+        kafkaContainer.followOutput(logConsumer);
+        pythonContainer.followOutput(logConsumer);
+
+    }
+}
